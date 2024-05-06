30c5fbb4ea1cb5584fa3f82fd3c5c21025be91f4
Ali Ensar Can <aliensarcan0@gmail.com>
2024-05-06 17:53:36 +0300

    category and products created

---
+++ FormsApp/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace FormsApp.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string Image { get; set; } = string.Empty;
+        public bool IsActive { get; set; }
+        public int Category { get; set; }
+
+
+
+    }
+}
