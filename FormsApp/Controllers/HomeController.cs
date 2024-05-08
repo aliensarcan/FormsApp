fa5dce19129e1e7f1bcaed58df526dabd1cee068
Ali Ensar Can <aliensarcan0@gmail.com>
2024-05-08 13:38:49 +0300

    search and filtering sections are completed

--- FormsApp/Controllers/HomeController.cs
+++ FormsApp/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using FormsApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Diagnostics;
 
 namespace FormsApp.Controllers
@@ -13,9 +14,29 @@ namespace FormsApp.Controllers
            
         }
 
-        public IActionResult Index()
+        public IActionResult Index(String searchString, string category)
+
         {
-            return View(Repository.Products);
+            var products = Repository.Products;
+            if(!string.IsNullOrEmpty(searchString))
+            {
+                ViewBag.SearchString = searchString;
+                products=products.Where(p  => p.Name.ToLower().Contains(searchString)).ToList();
+
+            }
+            if (!string.IsNullOrEmpty(category)&& category!="0") 
+            {
+                products = products.Where(p => p.CategoryId == int.Parse(category)).ToList();
+            }
+            //ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name");
+
+            var model = new ProductViewModel
+            {
+                Products = products,
+                Categories = Repository.Categories,
+                SelectedCategory = category
+            };
+            return View(model);
         }
 
         public IActionResult Privacy()
--- FormsApp/Models/Product.cs
+++ FormsApp/Models/Product.cs
@@ -6,7 +6,11 @@ namespace FormsApp.Models
     {
         [Display(Name="Urun Id")]
         public int ProductId { get; set; }
+       
+        [Display(Name = "Urun Ad")]
         public string Name { get; set; } = string.Empty;
+
+        [Display(Name = "Urun Fiyat")]
         public decimal Price { get; set; }
         public string Image { get; set; } = string.Empty;
         public bool IsActive { get; set; }
