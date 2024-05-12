eae2dbe9b57b297e54232bba42bd764fe38276e6
Ali Ensar Can <aliensarcan0@gmail.com>
2024-05-12 23:40:10 +0300

    created page and filtering features added

--- FormsApp/Controllers/HomeController.cs
+++ FormsApp/Controllers/HomeController.cs
@@ -38,12 +38,26 @@ namespace FormsApp.Controllers
             };
             return View(model);
         }
-
-        public IActionResult Privacy()
+        [HttpGet]
+        public IActionResult Create()
         {
+            ViewBag.Categories= new SelectList(Repository.Categories, "CategoryId", "Name");
             return View();
         }
+        [HttpPost]
+        public IActionResult Create(Product model)
 
-        
+        {
+            if (ModelState.IsValid)
+            {
+                model.ProductId=Repository.Products.Count + 1;
+                Repository.CreateProduct(model);
+                return RedirectToAction("Index");
+            }
+            ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name");
+            return View(model);
+        }
+
+
     }
 }
--- FormsApp/Models/Product.cs
+++ FormsApp/Models/Product.cs
@@ -1,19 +1,35 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
 
 namespace FormsApp.Models
 {
     public class Product
     {
         [Display(Name="Urun Id")]
+        
         public int ProductId { get; set; }
+        
+        [Required(ErrorMessage ="ÜRÜN ADI BOŞ GEÇİLEMEZ")]
        
         [Display(Name = "Urun Ad")]
-        public string Name { get; set; } = string.Empty;
 
-        [Display(Name = "Urun Fiyat")]
-        public decimal Price { get; set; }
-        public string Image { get; set; } = string.Empty;
+        public string? Name { get; set; } = string.Empty;
+        [Required(ErrorMessage ="ÜRÜN FİYATI BOŞ GEÇİLEMEZ")]
+        [Range(0,100000) ]
+        [Display(Name = "Fıyat")]
+        public decimal? Price { get; set; }
+        
+        
+        [Required(ErrorMessage ="RESİM BOŞ GEÇİLEMEZ")]
+        [Display(Name ="Resım")]
+        public string? Image { get; set; } = string.Empty;
+
+        
         public bool IsActive { get; set; }
+
+
+        
+        [Required(ErrorMessage ="KATEGORİ BOŞ GEÇİLEMEZ")]
         public int CategoryId { get; set; }
 
 
